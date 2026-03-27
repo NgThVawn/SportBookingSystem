9495d2942e5ca7d50b15bb6fc85767139e596f47
NgThVawn <nguyenthanhvane@gmail.com>
2026-03-27 14:16:05 +0700

    Khoi tao du an

---
+++ src/main/java/J2EE/SportBooingSystem/SportBooingSystemApplication.java
@@ -0,0 +1,13 @@
+package J2EE.SportBooingSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportBooingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SportBooingSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/J2EE/SportBooingSystem/SportBooingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package J2EE.SportBooingSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SportBooingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
