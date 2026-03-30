8007bb4cfdb9bddf420eaa4b414569434d9b3b7f
Trung0btCode <cutrung2k4@gmail.com>
2026-03-30 14:31:48 +0700

    Chỉnh sửa giao header và footer

--- src/main/java/J2EE/SportBooingSystem/SportBooingSystemApplication.java
+++ src/main/java/J2EE/SportBooingSystem/SportBooingSystemApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SportBooingSystemApplication {
+public class    SportBooingSystemApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SportBooingSystemApplication.class, args);
